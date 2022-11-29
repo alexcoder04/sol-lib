3aa0534743f1a03313a3a2a75e44de71e19a877c
alexcoder04 <alexcoder04@protonmail.com>
2022-11-29 18:50:58 +0100

    modern and classic colorscheme for dialog

--- library/dialog/drawing.lua
+++ library/dialog/drawing.lua
@@ -1,4 +1,44 @@
 
+-- classic colorscheme
+dialogColors = {
+    w = {
+        shadow = {100, 100, 100},
+        border = {128, 128, 128},
+        title = {255, 255, 255},
+        title_font = {"sansserif", "r", 10},
+        background = {224, 224, 224},
+        header_base = 32,
+        shadow_full_blurred = false
+    },
+    e = {
+        background = {255, 255, 255},
+        border = {0, 0, 0},
+        bar = {100, 100, 100}
+    },
+    text = {0, 0, 0},
+    focused = {50, 150, 190},
+}
+
+-- modern colorscheme
+--dialogColors = {
+--    w = {
+--        shadow = {240, 240, 240},
+--        border = {255, 255, 255},
+--        title = {0, 0, 0},
+--        title_font = {"sansserif", "b", 12},
+--        background = {255, 255, 255},
+--        header_base = nil,
+--        shadow_full_blurred = true
+--    },
+--    e = {
+--        background = {255, 255, 255},
+--        border = {0, 0, 0},
+--        bar = {0, 0, 0}
+--    },
+--    text = {100, 100, 100},
+--    focused = {45, 95, 250}
+--}
+
 function Lib.Dialog.dialogBox.paint(gc,dialogBox,windowID)
     local sizeX,sizeY
     if not dialogBox.size then
@@ -40,12 +80,12 @@ end
 function Lib.Dialog._paint_label(gc,label,x,y)
     gc:setFont("sansserif","r",10)
     if label.color then
-        gc:setColorRGB(0,0,0)
+        gc:setColorRGB(unpack(dialogColors.text))
         gc:fillRect(x+label.x,y+label.y,30,20)
         gc:setColorRGB(unpack(label.color))
         gc:fillRect(x+label.x+1,y+label.y+1,28,18)
     else
-        gc:setColorRGB(0,0,0)
+        gc:setColorRGB(unpack(dialogColors.text))
         gc:drawString(label.text,x+label.x,y+label.y,"top")
     end
 end
@@ -65,26 +105,30 @@ function Lib.Dialog._paint_text_box(gc,textBox,x,y,selected)
         textBox.setCursor=nil
     end
     if selected then
-        gc:setColorRGB(50,150,190)
+        gc:setColorRGB(unpack(dialogColors.focused))
         gc:fillRect(x+textBox.x-2,y+textBox.y-2,textBox.sizeX+5,27)
     end
-    gc:setColorRGB(255,255,255)
+    gc:setColorRGB(unpack(dialogColors.e.background))
     gc:fillRect(x+textBox.x,y+textBox.y,textBox.sizeX,22)
-    gc:setColorRGB(0,0,0)
+    gc:setColorRGB(unpack(dialogColors.e.border))
     gc:drawRect(x+textBox.x,y+textBox.y,textBox.sizeX-1,22)
     gc:setFont("sansserif","r",10)
+    gc:setColorRGB(unpack(dialogColors.text))
     gc:drawString(textBox.text,x+textBox.x+3,y+textBox.y+1,"top")
     if selected then
         gc:fillRect(gc:getStringWidth(string.usub(textBox.text,1,textBox.cursor))+x+textBox.x+3,y+textBox.y+2,1,19)
     end
 end
 
+newColor={225,170,0}
+
 function Lib.Dialog._paint_color_slider(gc,slider,x,y,selected)
+    -- focused
     if selected then
-        gc:setColorRGB(50,150,190)
+        gc:setColorRGB(unpack(dialogColors.focused))
         gc:fillRect(x+slider.x-2,y+slider.y-2,72,24)
     end
-    gc:setColorRGB(0,0,0)
+    gc:setColorRGB(unpack(dialogColors.e.border))
     gc:fillRect(x+slider.x,y+slider.y,68,20)
     for i=0,63 do
         gc:setColorRGB(slider.color=="red" and i*4 or newColor[1],slider.color=="green" and i*4 or newColor[2],slider.color=="blue" and i*4 or newColor[3])
@@ -95,15 +139,19 @@ function Lib.Dialog._paint_color_slider(gc,slider,x,y,selected)
 end
 
 function Lib.Dialog._paint_list(gc,list,x,y,selected)
+    -- focus
     if selected then
-        gc:setColorRGB(50,150,190)
+        gc:setColorRGB(unpack(dialogColors.focused))
         gc:fillRect(x+list.x-2,y+list.y-2,list.sizeX+4,list.sizeY+4)
     end
-    gc:setColorRGB(0,0,0)
+    -- border
+    gc:setColorRGB(unpack(dialogColors.e.border))
     gc:fillRect(list.x+x,list.y+y,list.sizeX,list.sizeY)
-    gc:setColorRGB(255,255,255)
+    -- background
+    gc:setColorRGB(unpack(dialogColors.e.background))
     gc:fillRect(list.x+1+x,list.y+1+y,list.sizeX-2,list.sizeY-2)
-    gc:setColorRGB(100,100,100)
+    -- scroll bar
+    gc:setColorRGB(unpack(dialogColors.e.bar))
     gc:drawImage(Lib.Dialog._img.upButton,list.x+x+list.sizeX-14,y+list.y+3)
     gc:drawImage(Lib.Dialog._img.downButton,list.x+x+list.sizeX-14,y+list.y+list.sizeY-13)
     gc:drawRect(list.x+x+list.sizeX-14,y+list.y+15,10,list.sizeY-31)
@@ -128,14 +176,15 @@ function Lib.Dialog._paint_list(gc,list,x,y,selected)
         local scrollBarSize=(list.sizeY-31)*list.sizeY/(#list.elements*fontHeight)
         gc:fillRect(list.x+x+list.sizeX-14,y+list.y+15+list.scroll*(list.sizeY-31)/#list.elements,11,scrollBarSize)
     end
-    gc:setColorRGB(0,0,0)
+    -- elements
+    gc:setColorRGB(unpack(dialogColors.text))
     local step=0
     for i=list.scroll+1,list.scroll+capacity do
         if list.elements[i] then
             if list.selected==i then
-                gc:setColorRGB(unpack(selected and {50,150,190} or {200,200,200}))
+                gc:setColorRGB(unpack(selected and dialogColors.focused or dialogColors.w.background))
                 gc:fillRect(list.x+x+1,list.y+y+step*fontHeight+1,list.sizeX-16,fontHeight-2)
-                gc:setColorRGB(0,0,0)
+                gc:setColorRGB(unpack(dialogColors.text))
             end
             gc:drawString(list.elements[i],list.x+x+3,list.y+y+step*fontHeight,"top")
             step=step+1
@@ -163,18 +212,22 @@ function Lib.Dialog._paint_buttons(gc,buttons,sizeX,sizeY,windowID)
         buttons=Lib.Dialog._windows[windowID].buttons
     end
     for i,e in pairs(buttons) do
-        gc:setColorRGB(136,136,136)
+        -- border
+        gc:setColorRGB(unpack(dialogColors.w.border))
         gc:fillRect(e.pos,y+sizeY+9,e.size,23)
         gc:fillRect(e.pos+1,y+sizeY+8,e.size-2,25)
         gc:fillRect(e.pos+2,y+sizeY+7,e.size-4,27)
-        gc:setColorRGB(255,255,255)
+        -- background
+        gc:setColorRGB(unpack(dialogColors.e.background))
         gc:fillRect(e.pos+2,y+sizeY+9,e.size-4,23)
-        gc:setColorRGB(0,0,0)
+        -- text
+        gc:setColorRGB(unpack(dialogColors.text))
         gc:drawString(e[1],e.pos+5,y+sizeY+20,"middle")
     end
+    -- focused border
     if Lib.Dialog.focus<0 and windowID==Lib.Dialog.NbWindows() then
         local button=buttons[-Lib.Dialog.focus]
-        gc:setColorRGB(50,150,190)
+        gc:setColorRGB(unpack(dialogColors.focused))
         gc:drawRect(button.pos-3,y+sizeY+4,button.size+5,32)
         gc:drawRect(button.pos-2,y+sizeY+5,button.size+3,30)
     end
@@ -182,30 +235,52 @@ end
 
 function Lib.Dialog._paint_text_area(gc,text,sizeX,sizeY)
     local x,y=(platform.window:width()-sizeX)/2,(platform.window:height()-sizeY-15)/2
-    gc:setColorRGB(128,128,128)
+    -- border
+    gc:setColorRGB(unpack(dialogColors.w.border))
     gc:drawRect(x+6,y+6,sizeX-13,sizeY-13)
-    gc:setColorRGB(0,0,0)
+    -- text
+    gc:setColorRGB(unpack(dialogColors.text))
     gc:setFont("sansserif","r",10)
     Lib.Gui.MultiLineStr.draw(gc,text,x+12,y+9)
 end
 
 function Lib.Dialog._paint_window_bg(gc,name,sizeX,sizeY)
     local x,y=(platform.window:width()-sizeX)/2,(platform.window:height()-sizeY-15)/2
-    gc:setColorRGB(100,100,100)
-    gc:fillRect(x-1,y-23,sizeX+4,sizeY+65)
-    gc:fillRect(x,y-22,sizeX+4,sizeY+65)
-    gc:fillRect(x+1,y-21,sizeX+4,sizeY+65)
-    gc:setColorRGB(128,128,128)
-    gc:fillRect(x-2,y-24,sizeX+4,sizeY+65)
-    for i=1,22 do
-        gc:setColorRGB(32+i*3,32+i*3,32+i*3)
-        gc:fillRect(x,y+i-23,sizeX,1)
+    -- shadow
+    gc:setColorRGB(unpack(dialogColors.w.shadow))
+    if dialogColors.w.shadow_full_blurred then
+        gc:fillRect(x-5,y-27,sizeX+10,sizeY+71)
+        gc:setColorRGB(dialogColors.w.shadow[1]-20, dialogColors.w.shadow[2]-20, dialogColors.w.shadow[3]-20)
+        gc:fillRect(x-4,y-26,sizeX+8,sizeY+69)
+        gc:setColorRGB(dialogColors.w.shadow[1]-40, dialogColors.w.shadow[2]-40, dialogColors.w.shadow[3]-40)
+        gc:fillRect(x-3,y-25,sizeX+6,sizeY+67)
+    else
+        gc:fillRect(x-1,y-23,sizeX+4,sizeY+65)
+        gc:fillRect(x,y-22,sizeX+4,sizeY+65)
+        gc:fillRect(x+1,y-21,sizeX+4,sizeY+65)
     end
-    gc:setColorRGB(255,255,255)
-    gc:setFont("sansserif","r",10)
+    -- border
+    gc:setColorRGB(unpack(dialogColors.w.border))
+    gc:fillRect(x-2,y-24,sizeX+4,sizeY+65)
+    -- header
+    if dialogColors.w.header_base == nil then
+        gc:setColorRGB(unpack(dialogColors.w.background))
+        gc:fillRect(x,y-1,sizeX,1)
+    else
+        for i=1,22 do
+            gc:setColorRGB(dialogColors.w.header_base+i*3,dialogColors.w.header_base+i*3,dialogColors.w.header_base+i*3)
+            gc:fillRect(x,y+i-23,sizeX,1)
+        end
+    end
+    -- title
+    gc:setColorRGB(unpack(dialogColors.w.title))
+    gc:setFont(unpack(dialogColors.w.title_font))
     gc:drawString(name,x+4,y-9,"baseline")
-    gc:setColorRGB(224,224,224)
+    gc:setFont("sansserif","r",10)
+    -- bg
+    gc:setColorRGB(unpack(dialogColors.w.background))
     gc:fillRect(x,y,sizeX,sizeY+39)
-    gc:setColorRGB(128,128,128)
+    -- separator bar
+    gc:setColorRGB(unpack(dialogColors.w.border))
     gc:fillRect(x+6,y+sizeY,sizeX-12,2)
 end
