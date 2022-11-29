785236401298a46901d524ae50cf158ae18a4741
alexcoder04 <alexcoder04@protonmail.com>
2022-11-29 16:17:03 +0100

    new Lib.Internal functions

--- library/internal.lua
+++ library/internal.lua
@@ -8,3 +8,37 @@ end
 function Lib.Internal.IsRunnable(var)
     return (var ~= nil) and (type(var) == "function")
 end
+
+function Lib.Internal.ArrayContains(array, element)
+    for _, v in ipairs(array) do
+        if v == element then return true end
+    end
+    return false
+end
+
+function Lib.Internal.IsDigit(char)
+    for _, d in ipairs({"1", "2", "3", "4", "5", "6", "7", "8", "9", "0"}) do
+        if char == d then return true end
+    end
+    return false
+end
+
+function Lib.Internal.NormalizeNumber(num)
+    local offset
+    while num:sub(1,2) == "--" do
+        num = num:sub(2,#(num))
+    end
+    if num:sub(1,1) == "-" then
+        offset = 2
+    else
+        offset = 1
+    end
+
+    while num:sub(offset,offset) == "0" do
+        num = num:sub(offset+1,#(num))
+    end
+
+    if num == "" then return "0" end
+    if num:sub(1,1) == "." then num = "0" .. num end
+    return num
+end
